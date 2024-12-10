4c71d49074e47b3e1804c18f7606948ca49d1a23
sara4ness <saraholteness@gmail.com>
2024-12-10 15:11:38 +0000

    Second commit, circles moving with ouse, random colours
    Made  circles moving with mouse, also added random colours.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
   createCanvas(400, 400);
+  background(0);
+
 }
 
 function draw() {
-  background(220);
+  let r = random(255);
+  let g = random(255);
+  let b = random(255);
+  fill(r, g, b);
+  noStroke();
+circle(mouseX, mouseY, 50);
 }
